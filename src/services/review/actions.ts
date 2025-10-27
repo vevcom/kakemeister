451df05f92fcdf1467f2223803bc54196c0bdc62
Ludvig <ludvigso@stud.ntnu.no>
2025-10-27 19:19:07 +0100

    Diverse endringer paa min lokale branch

--- src/services/review/actions.ts
+++ src/services/review/actions.ts
@@ -26,3 +26,27 @@ export async function get_rating(cakeId:number) {
     }
 
 }
+
+export async function userratings(username:string) {
+    const ratings = await prisma.review.aggregate({
+        where:{
+            cakeId:cakeId
+        },
+        _avg:{
+            rating:true
+        },
+        _count:{
+            rating:true
+        }
+    })
+
+    if(!rating){
+        return;
+    }
+
+    return {
+        avgRating: rating._avg.rating,
+        countRating: rating._count.rating
+    }
+
+}
