3c4d1809a62b0dfcaeba9756f774e2cc36aea605
Ludvig Sørensen <ludvigso@stud.ntnu.no>
2026-02-09 17:47:22 +0100

    jobber med login

--- src/app/login-page/login.tsx
+++ src/app/login-page/login.tsx
@@ -1,1 +1,26 @@
-//login-funksjonen må være her
+//login-funksjonen må være her'
+
+
+const login = async (brukernavn) => {
+    
+
+    if (!brukernavn) {
+        alert("Du må skrive inn et brukernavn.");
+        return;
+    }
+
+    const user = await prisma.user.findFirst({
+      where: {
+        username:brukernavn
+      }  
+    })
+
+    if (!user) {
+      alert("Dette er ikke et gyldig brukernavn")
+    } else {
+      localStorage.setItem("brukernavn", brukernavn);
+      alert("Du er logget inn")
+      
+  }
+
+}
--- src/app/login-page/page.tsx
+++ src/app/login-page/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 import styles from "./page.module.scss"
 import { useState, FormEvent, ChangeEvent } from "react"
+import login from "./login"
 import { useRouter } from "next/navigation"
 import { add_cake } from "@/services/adding-cake/actions"
 import { get_users_by_name } from "@/services/user/actions"
@@ -10,34 +11,10 @@ type bruker = {
   brukernavn: string
 }
 
-const login = async (brukernavn) => {
-    
 
-    if (!brukernavn) {
-        alert("Du må skrive inn et brukernavn.");
-        return;
-    }
-
-    const user = await prisma.user.findFirst({
-      where: {
-        username:brukernavn
-      }  
-    })
-
-    if (!user) {
-      alert("Dette er ikke et gyldig brukernavn")
-    } else {
-      localStorage.setItem("brukernavn", brukernavn);
-      alert("Du er logget inn")
-      
-  }
-
-}
 
 export default function LoginPage() {
   const [tekst, setTekst] = useState("")
-  
-  
 
   return (
     <div>
