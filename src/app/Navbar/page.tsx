0f96386b82f9a1bb84503b2dd08d2893586c1c05
Ludvig Sørensen <ludvigso@stud.ntnu.no>
2026-03-16 20:19:34 +0100

    fourth stage to navbar from scratch

--- src/app/Navbar/page.tsx
+++ src/app/Navbar/page.tsx
@@ -1,14 +1,16 @@
-import "./page.module.scss"
+import styles from "./page.module.scss"
+import Link from "next/link"
 
 export default function Nav() {
     return(
         <div id="Header">
-            <li className="header_li"><a href="index.html">Hjem</a></li>
-            <li className="header_li"><a href="Cookies.html">Cookies</a></li>
-            <li id="highlighted"><a href="Phishing.html">Phishing</a></li>
-            <li className="header_li"><a href="Testen.html">Ta quizen</a></li>
-            <li className="header_li"><a href="kommentarer.html">Kommentarer</a></li>
-            <li className="header_li"><a href="login.html">Logg inn</a></li>
+            <ol>
+                <li className="header_item"><Link href="/">Hjem</Link></li>
+                <li className="header_item"><Link href="/">Hjem</Link></li>
+                <li className="header_item"><Link href="/">Hjem</Link></li>
+                <li className="header_item"><Link href="/">Hjem</Link></li>
+                <li className="header_item"><Link href="/">Hjem</Link></li>
+            </ol>
         </div>
     )
 }
