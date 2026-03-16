426ad73fcb710733c2a1fe9f99d10de2ac44fef7
Ludvig Sørensen <ludvigso@stud.ntnu.no>
2026-03-16 19:48:26 +0100

    tredje steget mot bra navbar

--- src/app/Navbar/page.tsx
+++ src/app/Navbar/page.tsx
@@ -1,7 +1,14 @@
+import "./page.module.scss"
+
 export default function Nav() {
     return(
-        <div>
-        En random div
+        <div id="Header">
+            <li className="header_li"><a href="index.html">Hjem</a></li>
+            <li className="header_li"><a href="Cookies.html">Cookies</a></li>
+            <li id="highlighted"><a href="Phishing.html">Phishing</a></li>
+            <li className="header_li"><a href="Testen.html">Ta quizen</a></li>
+            <li className="header_li"><a href="kommentarer.html">Kommentarer</a></li>
+            <li className="header_li"><a href="login.html">Logg inn</a></li>
         </div>
     )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,8 +26,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <Nav/>
         {children}
-        <Nav></Nav>
       </body>
     </html>
   );
