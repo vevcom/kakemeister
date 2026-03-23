a39a0add21fe5b08df33c10d7aadf4e40508af69
Ludvig Sørensen <ludvigso@stud.ntnu.no>
2026-03-23 19:08:30 +0100

    better color pallette

--- src/app/Navbar/page.tsx
+++ src/app/Navbar/page.tsx
@@ -5,10 +5,10 @@ export default function Nav() {
     return(
         <div className={styles.header}>
             <ol className={styles.navbar}>
-                <li className={styles.header_item}><Link href="/">Hjem</Link></li>
-                <li className={styles.header_item}><Link href="/Cake_review">Cake Review</Link></li>
-                <li className={styles.header_item}><Link href="/Statistikk">Statistikk</Link></li>
-                <li className={styles.header_item}><Link href="/Login_page">Login</Link></li>
+                <Link href="/"><li className={styles.header_item}>Hjem</li></Link>
+                <Link href="/Cake_review"><li className={styles.header_item}>Kakeanmeldelser</li></Link>
+                <Link href="/Statistikk"><li className={styles.header_item}>Statistikk</li></Link>
+                <Link href="/Login_page"><li className={styles.header_item}>Login</li></Link>
             </ol>
         </div>
     )
